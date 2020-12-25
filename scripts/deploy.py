54fb8fcf573406bd2c8543ceb66ab09ffaf69f1c
arvinxx <arvinx@foxmail.com>
2020-12-25 12:45:39 +0800

    :wrench: chore: 部署脚本

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -2,7 +2,11 @@ import os
 
 output = '../dist'
 generate = 'generate_html.py'
+# 清理前产物
 os.system(f"rm -rf {output}")
+# 生成 html
 os.system(f"python {generate}")
+# 部署
 os.system(f"ghp-import -n -p -f {output}")
+# 清理生成产物
 os.system(f"rm -rf {output}")
